0e8add4d329d8c3244081f67633dbd74c08b5a8d
Unknown <contact@ilyt.cc>
2018-12-15 19:26:48 +0000

    oopsie

--- helpers/webhookHelper.py
+++ helpers/webhookHelper.py
@@ -1,4 +1,4 @@
-from helpers.webhookHandler import Webhook
+from handlers.webhookHandler import Webhook
 
 def postWebhook(url, args=None):
     embed = Webhook(url, **args, footer="osu!Yozora :heart:")
