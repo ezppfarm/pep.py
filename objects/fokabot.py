ab4db29d956d40bc09f2983b315384d28a3985d0
depreciate <thecriti@gmail.com>
2019-03-02 10:12:48 +1030

    adasd

--- objects/fokabot.py
+++ objects/fokabot.py
@@ -20,7 +20,7 @@ def connect():
 	"""
 	token = glob.tokens.addToken(999)
 	token.actionID = actions.IDLE
-	text = "\nYozora made with love by Alicia & Sarah "
+	text = "    Yozora made with love by\nAlicia & Sarah "
 	token.actionText = text
 	token.country = 227
 	glob.streams.broadcast("main", serverPackets.userPanel(999))
